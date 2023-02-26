e815445596f43c908c6623213bd2a868b1c80062
ArushKumar01 <aruahkumar@gmail.com>
2023-02-26 08:06:03 +0000

    modified:   home/css/style.css 	modified:   home/index.html 	modified:   home/js/script.js

--- home/js/script.js
+++ home/js/script.js
@@ -13,3 +13,36 @@ let bell = document.querySelector('.notification');
 document.querySelector('#bell-icon').onclick = () => {
     bell.classList.toggle('active');
 }
+
+//Swiper
+const swiper = new Swiper('.trending-content', {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      // when window width is >= 480px
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      // when window width is >= 640px
+      1068: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+});
